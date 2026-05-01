ebb1e5acfd06470b1603a4137d79aca70c59ef72
tanjil-hosain <tanjilh85@gmail.com>
2026-05-01 21:49:19 +0600

    db-modify for sequre

--- config/db.php
+++ config/db.php
@@ -1,11 +1,20 @@
 <?php
+$host = 'localhost';
+$db   = 'bazaar_mama'; 
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-$connect = mysqli_connect("localhost", "root", "", "bazaar_mama");
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-if (!$connect) {
-    die("Connection Failed: " . mysqli_connect_error());
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-
-echo "Connection Successful";
-
 ?>
